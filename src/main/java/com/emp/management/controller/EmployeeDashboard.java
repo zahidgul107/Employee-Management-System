6d37e5876d6745dd3c02dfa7bebc4205decd4a43
ZAHID GUL <zahidgul107@gmail.com>
2023-01-07 15:10:29 +0530

    edited attendance_API and send token to API

--- src/main/java/com/emp/management/controller/EmployeeDashboard.java
+++ src/main/java/com/emp/management/controller/EmployeeDashboard.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -101,70 +102,97 @@ public class EmployeeDashboard {
 	@PostMapping("/saveAttendance")
 	public String saveAttendance(@RequestParam String token,@ModelAttribute("attendance") Attendance attendance, RedirectAttributes rd) {
 	//	System.err.println(token);
-		if (attendanceRepo.findByDate(attendance.getDate()) != null) {
+		List<Employee> employee = employeeRepo.findByToken(token);
+		for (Employee emp : employee) {
+			System.err.println(emp.getId());
+			long id = emp.getId();
+			if (attendanceRepo.findByEmployeeIdAndDate(id,attendance.getDate()) != null) {
+				rd.addAttribute("token", token);
+				rd.addFlashAttribute("fail", "Attendance already done");
+				return "redirect:/showAttendanceForm";
+			}
+		}
+		
+	/*	if (attendanceRepo.findByDate(attendance.getDate()) != null) {
 			rd.addAttribute("token", token);
 			rd.addFlashAttribute("fail", "Attendance already done");
 			return "redirect:/showAttendanceForm";
-		} else {
+		}	*/
+		 	
 			attendanceRepo.save(attendance);
 			rd.addFlashAttribute("success", "Attendance submitted successfull");
 			rd.addAttribute("token", token);
 			return "redirect:/employeeView";
-		}
+			 
 	}
 
 	@GetMapping("/showUpdateAttendanceForm/{id}")
-	public String showUpdateAttendanceForm(@RequestParam String token,@PathVariable(value = "id") long id, Model model) {
-		System.err.println(token);
+	public String showUpdateAttendanceForm(@RequestParam String token,@PathVariable(value = "id") long id, Model model,RedirectAttributes rd) {
+	//	System.err.println(token);
 		model.addAttribute("token", token);
 		String timeOut = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
-		List<Attendance> timeInAttendance = attendanceRepo.findByEmployeeIdAndTimeOutIsNull(id);
-		model.addAttribute("timeInAttendance", timeInAttendance);
-		for (Attendance attendance : timeInAttendance) {
-			System.err.println(attendance.getTimeIn());
-		}
-		Attendance attendance = new Attendance();
-		attendance.setTimeOut(timeOut);
-		model.addAttribute("attendance", attendance);
-	//	model.addAttribute("listEmployees", employeeRepo.findByTokenIsNotNull());
+		if (attendanceRepo.findByEmployeeIdAndTimeOutIsNotNull(id) != null) {
+			rd.addAttribute("token", token);
+			rd.addFlashAttribute("success", "Attendance updated already today!");
+			return "redirect:/employeeView";
+			
+		} else {
+			List<Attendance> timeInAttendance = attendanceRepo.findByEmployeeIdAndTimeOutIsNull(id);
+			model.addAttribute("timeInAttendance", timeInAttendance);
+			for (Attendance attendance : timeInAttendance) {
+				System.err.println(attendance.getTimeIn());
+			}
+			Attendance attendance = new Attendance();
+			attendance.setTimeOut(timeOut);
+			model.addAttribute("attendance", attendance);
+		//	model.addAttribute("listEmployees", employeeRepo.findByTokenIsNotNull());
 
-		return "update_attendance_form";
+			return "update_attendance_form";
+		}
+
 	}
 
 	@PostMapping("/updateAttendance")
-	public String saveUpdatedAttendance(@ModelAttribute("attendance") Attendance attendance, RedirectAttributes rd) {
+	public String saveUpdatedAttendance(@RequestParam String token,@ModelAttribute("attendance") Attendance attendance, RedirectAttributes rd) {
+		System.err.println(token);
 		Attendance att = attendanceRepo.findById(attendance.getId()).get();
 		if (att.getTimeOut() != null) {
 			rd.addFlashAttribute("success", "You have already taken attendance");
+			rd.addAttribute("token", token);
 			return "redirect:/employeeView";
 		} else {
-			System.err.println("hereeeeeeeeeeeeeeeeeeeeee");
+		//	System.err.println("hereeeeeeeeeeeeeeeeeeeeee");
 			attendanceRepo.save(attendance);
 			rd.addFlashAttribute("success", "Attendance updated successfully");
+			rd.addAttribute("token", token);
 			return "redirect:/employeeView";
 		}
 
 	}
 
 	@GetMapping("/showLeaveForm")
-	public String showLeaveForm(Model model) {
-
+	public String showLeaveForm(@RequestParam String token,Model model) {
+	//	System.err.println(token);
 		// create model attribute to bind form data
 		EmployeeLeave leave = new EmployeeLeave();
 		model.addAttribute("leave", leave);
 		List<Employee> listEmployees = employeeRepo.findByTokenIsNotNull();
 		model.addAttribute("listEmployees", listEmployees);
+		model.addAttribute("token", token);
 		return "leave_form";
 	}
 
 	@PostMapping("/approveLeave")
-	public String acceptLeave(@ModelAttribute("leave") EmployeeLeave leave, RedirectAttributes rd) {
-		System.err.println(leave.getFromDate());
+	public String acceptLeave(@RequestParam String token,@ModelAttribute("leave") EmployeeLeave leave, RedirectAttributes rd) {
+		System.err.println(token);
+	//	System.err.println(leave.getFromDate());
 		if (empLeaveRepo.findByFromDate(leave.getFromDate()) != null) {
 			rd.addFlashAttribute("fail", "Your have already submitted leave from this date");
+			rd.addFlashAttribute("token", token);
 			return "redirect:/showLeaveForm";
 		} else if (empLeaveRepo.findByToDate(leave.getToDate()) != null) {
 			rd.addFlashAttribute("fail", "you have already submitted leave to this date");
+			rd.addFlashAttribute("token", token);
 			return "redirect:/showLeaveForm";
 		}
 		
@@ -172,22 +200,24 @@ public class EmployeeDashboard {
 			leave.setStatus("created");
 			empLeaveRepo.save(leave);
 			rd.addFlashAttribute("success", "leave submitted successfull");
+			rd.addFlashAttribute("token", token);
 			return "redirect:/employeeView";
 		}
 	}
 
 	@GetMapping("/showEmployeeLeaveStatus")
-	public String showEmployeeLeaves() {
+	public String showEmployeeLeaves(@RequestParam String token,Model model) {
+		model.addAttribute("token", token);
 		return "employee_leave_view";
 	}
 
 	@GetMapping("/showLeaveStatus/{status}/{page}")
-	public String showEmployeeLeavedStatus(@PathVariable(value = "status") String status,@PathVariable(value = "page")int page, Model model) {
+	public String showEmployeeLeavedStatus(@RequestParam String token,@PathVariable(value = "status") String status,@PathVariable(value = "page")int page, Model model) {
 
 		ArrayList<Employee> list = new ArrayList<>();
-		List<Employee> listEmployees = employeeRepo.findByTokenIsNotNull();
+		List<Employee> listEmployees = employeeRepo.findByToken(token);
 		for (Employee employee : listEmployees) {
-		//	System.out.println(employee.getId());
+			System.out.println(employee.getId());
 			list.add(employee);
 		}	
 		Pageable pageable = PageRequest.of(page-1, 5);
@@ -197,6 +227,7 @@ public class EmployeeDashboard {
 		model.addAttribute("totalPages", listLeave.getTotalPages());
 		model.addAttribute("totalItems", listLeave.getTotalElements());
 		model.addAttribute("status", status);
+		model.addAttribute("token", token);
 		return "employee_leave_status";
 	}
 
